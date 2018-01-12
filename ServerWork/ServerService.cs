0394ad4922657f193d6836793947bbe832b78990
Iichi Kohuro <iichi.kohuro@gmail.com>
2018-01-12 15:38:05 +0300

    Add file via upload

---
+++ ServerWork/ServerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServerWork
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ServerService" в коде и файле конфигурации.
+    public class ServerService : IServerService
+    {
+
+        public string SendBids()
+        {
+            string mess = string.Empty;
+
+            mess = "Отправка заявок произведена успешно";
+
+            return mess;
+        }
+    }
+}
